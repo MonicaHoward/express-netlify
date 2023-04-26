de070783a2f11fd3f0c6f4ba97aebdd435a3341c
Monica <jay.m.howard@gmail.com>
2023-04-25 22:36:17 -0500

    added it ignore; updated server

--- server.js
+++ server.js
@@ -1,28 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
-const serverless = require('serverless-http');
 const ejs = require('ejs');
 const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
-
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
-
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-
-module.exports = app;
-module.exports.handler = serverless(app);
 
 const DB_URI = 'mongodb+srv://monicah2:bobdylan33@db1.zjskuo0.mongodb.net/?retryWrites=true&w=majority';
 const DB_NAME = 'marcbBlogDB';
